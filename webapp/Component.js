14ff3f4bbf014e9127e53f6875ef98e79c4ba564
syedtaj55319 <syedtaj.abap@gmail.com>
2024-09-30 18:17:12 +0300

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.project4abapodata.project4abapodata.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
